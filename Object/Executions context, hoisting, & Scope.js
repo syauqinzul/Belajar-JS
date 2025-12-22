0743b9037b9ee3caa96695a4319589a0b93c018a
syauqinzul <nuzulabdisyauqi@gmail.com>
2025-12-23 02:27:31 +0800

    Executions context, hoisting, & Scope

--- Object/Executions context, hoisting, & Scope.js
+++ Object/Executions context, hoisting, & Scope.js
@@ -3,7 +3,7 @@
 // execution context
 console.log("Execution Context");
 function a() {
-  console.log("Inside function a"); 
+  console.log("Inside function a");
   function b() {
     console.log("Inside function b");
   }
@@ -12,7 +12,7 @@ function a() {
 
 a();
 
-// hoisting 
+// hoisting
 console.log("\nHoisting Example");
 console.log(x);
 var x = 5;
@@ -20,20 +20,20 @@ console.log(x);
 
 // console.log(y); // Uncommenting this line will cause a ReferenceError
 let y = 10;
-console.log(y); 
+console.log(y);
 
 // scope
 console.log("\nScope Example");
 function outerFunction() {
-  let outerVar = "I'm outside!";    
+  let outerVar = "I'm outside!";
   function innerFunction() {
     let innerVar = "I'm inside!";
-    console.log(outerVar); 
-    console.log(innerVar); 
-  } 
+    console.log(outerVar);
+    console.log(innerVar);
+  }
   innerFunction();
   // console.log(innerVar); // Uncommenting this line will cause a ReferenceError
-} 
+}
 
 outerFunction();
 
@@ -42,18 +42,18 @@ outerFunction();
 let globalVar = "I'm global!";
 
 function checkGlobal() {
-  console.log(globalVar); 
-}   
+  console.log(globalVar);
+}
 
 checkGlobal();
 
-// block scope  
+// block scope
 
-console.log("\nBlock Scope Example");   
+console.log("\nBlock Scope Example");
 {
   let blockVar = "I'm block scoped!";
-  console.log(blockVar); 
-}   
+  console.log(blockVar);
+}
 
 // console.log(blockVar); // Uncommenting this line will cause a ReferenceError
 
@@ -61,8 +61,8 @@ console.log("\nBlock Scope Example");
 console.log("\nFunction Scope Example");
 function functionScopeExample() {
   var functionVar = "I'm function scoped!";
-  console.log(functionVar); 
-} 
+  console.log(functionVar);
+}
 
 functionScopeExample();
 // console.log(functionVar); // Uncommenting this line will cause a ReferenceError
@@ -88,3 +88,21 @@ functionScopeExample();
 // let syauqi = new mahasiswa("Syauqi", 10);
 // let palji = new mahasiswa("Palji", 20);
 
+
+//execution phase
+
+console.log(sayHello());
+
+var nama = "Syauqi";
+var umur = 19;
+
+function sayHello() {
+  return `Halo nama saya ${nama}, umur saya ${umur} tahun.`;
+}
+
+// hoisting phase
+// var nama;
+// var umur;
+// function sayHello() {
+//   return `Halo nama saya ${nama}, umur saya ${umur} tahun.`;
+// }
